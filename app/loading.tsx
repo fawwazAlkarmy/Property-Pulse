25d9089dde7490573c9e0b611c183a0242f65d00
fawwazAlkarmy <alkarmyfawwaz@gmail.com>
2024-03-04 13:19:16 +0300

    Build initial schema and connect to db with prisma

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,4 +1,24 @@
-const loading = () => {
-  return <div>loading</div>;
+"use client";
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override = {
+  display: "block",
+  margin: "100px auto",
 };
-export default loading;
+
+interface Props {
+  loading: boolean;
+}
+
+const LoadingPage = ({ loading }: Props) => {
+  return (
+    <ClipLoader
+      color="#3b82f6"
+      loading={loading}
+      cssOverride={override}
+      size={150}
+      aria-label="Loading Spinner"
+    />
+  );
+};
+export default LoadingPage;
